8e3219630b5d8960f37ddf369667083c5d5d59b0
Kevin Ordoñez <kevin.arian3003@gmail.com>
2021-07-20 17:12:05 -0300

    front

--- client/src/modules/search-bar/search-bar.component.jsx
+++ client/src/modules/search-bar/search-bar.component.jsx
@@ -10,17 +10,78 @@ function SearchBar(props){
         setSearchBarState(e.target.value)
     }
 
+    const sortBy = (order , type , array )=>{
+        const aux = [...array]
+        if( order === "ASC"){
+            if (type === "NAME"){
+                aux.sort(function (a,b){
+                    let nameA = a.name.toUpperCase()
+                    let nameB = b.name.toUpperCase()
+
+                    if (nameA < nameB) {
+                        return -1;
+                      }
+                      if (nameA > nameB) {
+                        return 1;
+                      }
+                      return 0;
+                })
+            }
+            else if(type === "WEIGHT"){
+                aux.sort(function (a,b){
+                    let weightA = Number.parseInt([a.weight.split(" ")][0])
+                    let weightB = Number.parseInt([b.weight.split(" ")][0])
+                    if (!weightA && weightA !== 0) weightA = 250
+                    if (!weightB && weightB !== 0) weightA = 250 
+                    return weightA - weightB;
+                })
+            }
+        }
+        if( order === "DESC"){
+            if (type === "NAME"){
+                aux.sort(function (a,b){
+                    let nameA = a.name.toUpperCase()
+                    let nameB = b.name.toUpperCase()
+
+                    if (nameA > nameB) {
+                        return -1;
+                      }
+                      if (nameA < nameB) {
+                        return 1;
+                      }
+                      return 0;
+                })
+            }
+            else if(type === "WEIGHT"){
+                aux.sort(function (a,b){
+                    let weightA = Number.parseInt([a.weight.split(" ")][0])
+                    let weightB = Number.parseInt([b.weight.split(" ")][0])
+                    if (!weightA && weightA !== 0) weightA = 250
+                    if (!weightB && weightB !== 0) weightA = 250 
+                    return weightB - weightA;
+                })
+            }
+        }
+        return aux
+    }
+
+
+    const filterBy = ( origin , temperament , sorted) =>{
+        const aux = sorted.filter()
+    }
+
     useEffect(()=>{
         if(searchBarState !== ""){
             const filteredState = stateToFilter.filter(item =>{
                 //console.log("ESTO ES ITEM NAME ", item.name , "ESTO ES TARGET VALUE ", e.target.value)
+
+
                 return item.name.toLowerCase().includes(searchBarState.toLowerCase())
             })
-            console.log("ESTO ES FILTERED STATE",filteredState)
             setState(filteredState)
         }
         
-    },[searchBarState])
+    },[searchBarState, origin , type , order , temperamentFilter])
 
 
     return(
